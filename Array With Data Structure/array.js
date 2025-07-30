674322a729c0e54cc394246a0e780c7f7a496f28
Krish-pajiyawala <pajiyawalakrish@gmail.com>
2025-07-30 10:47:25 +0530

    project

---
+++ Array With Data Structure/array.js
@@ -0,0 +1,16 @@
+// Array operation
+
+// let array = [1, 2, 3, 4, 5];
+
+// array.push(6) // [1, 2, 3, 4, 5, 6]
+// array.pop() // [1, 2, 3, 4]
+// array.shift() // [2, 3, 4, 5]
+// array.unshift(1) // [1, 2, 3, 4, 5]
+// array.splice(1, 2, 10, 20) // [1, 10, 20, 4, 5]
+// array.slice(1, 3) // [2, 3]
+// console.log(array);
+
+// Linear Search
+
+let array = [1, 2, 3, 4, 5];
+
