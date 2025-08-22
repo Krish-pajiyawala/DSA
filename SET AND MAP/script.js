9a4d41cb7f31fd4822821c156b2be62c4e72a727
Krish-pajiyawala <pajiyawalakrish@gmail.com>
2025-08-22 10:50:05 +0530

    add

---
+++ SET AND MAP/script.js
@@ -0,0 +1,15 @@
+// let data = new Set([1,2,2,3,3,4,5,6,1])
+// data.add(10)
+// data.add(10)
+// data.clear()
+// data.delete(10)
+// console.log(data.entries());
+// data.forEach((e)=> console.log(e))
+// console.log(data.has(50));
+// console.log(data.keys());
+// console.log(data.values());
+// console.log(data.size);
+
+
+// console.log(data);
+
