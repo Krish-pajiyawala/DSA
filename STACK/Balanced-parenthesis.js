9d4716415dfa9e9fc4b9eb83892cf2a434ba25a4
Krish-pajiyawala <pajiyawalakrish@gmail.com>
2025-08-26 13:19:10 +0530

    add

---
+++ STACK/Balanced-parenthesis.js
@@ -0,0 +1,31 @@
+// 2.Balanced parenthesis
+
+function isBalanced(str) {
+  let stack = [];
+  let pairs = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (char === '(' || char === '{' || char === '[') {
+      stack.push(char);
+    } 
+    else if (char === ')' || char === '}' || char === ']') {
+      if (stack.length === 0 || stack[stack.length - 1] !== pairs[char]) {
+        return false; 
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isBalanced("{[()]}"));   
+console.log(isBalanced("{[(])}"));  
+console.log(isBalanced("((()))"));   
+console.log(isBalanced("((())"));    
---
+++ STACK/Next-greater.js
@@ -0,0 +1,24 @@
+// 3.next Greater Elements
+
+function nextGreaterElements(arr) {
+  let n = arr.length;
+  let result = new Array(n).fill(-1);  
+  let stack = []; 
+
+  for (let i = n - 1; i >= 0; i--) {
+    while (stack.length > 0 && stack[stack.length - 1] <= arr[i]) {
+      stack.pop();
+    }
+
+    if (stack.length > 0) {
+      result[i] = stack[stack.length - 1];
+    }
+
+    stack.push(arr[i]);
+  }
+
+  return result;
+}
+
+console.log(nextGreaterElements([4, 5, 2, 25])); 
+console.log(nextGreaterElements([13, 7, 6, 12]));
